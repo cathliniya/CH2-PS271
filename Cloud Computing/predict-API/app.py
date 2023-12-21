7e0800e68a6234421cf18afdeb87b7a7991b7742
cathliniya <122766475+cathliniya@users.noreply.github.com>
2023-12-21 23:35:58 +0700

    Add files via upload

---
+++ Cloud Computing/predict-API/app.py
@@ -0,0 +1,71 @@
+import os
+from google.cloud import storage
+import tensorflow as tf
+from io import BytesIO
+from flask import Flask, request, jsonify
+from keras.models import load_model
+import numpy as np
+from tensorflow.keras.applications.mobilenet import preprocess_input
+
+app = Flask(__name__)
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'handspeak-credentials.json' 
+storage_client = storage.Client()
+
+def req(y_true, y_pred):
+    req = tf.metrics.req(y_true, y_pred)[1]
+    tf.keras.backend.get_session().run(tf.local_variables_initializer())
+    return req
+
+model = load_model('my_model_fix.h5', custom_objects={'req': req})
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        try:
+            image_bucket = storage_client.get_bucket(
+                'dataset_alphabet')
+            filename = request.json['filename']
+            img_blob = image_bucket.blob('predict_uploads/' 'A/', + filename) #SESUAIKAN
+            img_path = BytesIO(img_blob.download_as_bytes())
+        except Exception:
+            respond = jsonify({'message': 'Error loading image file'})
+            respond.status_code = 400
+            return respond
+
+        img = tf.keras.utils.load_img(img_path, target_size=(224, 224))
+        x = tf.keras.utils.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        images = np.vstack([x])
+
+        # model predict
+        pred_huruf = model.predict(images)
+        # find the max prediction of the image
+        maxx = pred_huruf.max()
+
+        huruf = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+        # for respond output from prediction if predict <=0.4
+        if maxx <= 0.75:
+            respond = jsonify({
+                'message': 'tidak terdeteksi'
+            })
+            respond.status_code = 400
+            return respond
+
+        result = {
+            "nama_huruf": nama_huruf[np.argmax(pred_huruf)],
+            "deskripsi": deskripsi[np.argmax(pred_huruf)]
+        }
+
+        respond = jsonify(result)
+        respond.status_code = 200
+        return respond
+
+    return 'OK'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
